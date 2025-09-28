3165a815bb7a31b38e034d3d451fb66847b1ec8f
gzlm163 <marusyagzl@gmail.com>
2025-09-28 17:33:48 +0700

    Update a2.cpp

--- a2.cpp
+++ a2.cpp
@@ -1,6 +1,6 @@
 /**********************
  * Автор: Гинжул Мария*
- * Дата: 25.09.2025   *
+ * Дата: 28.09.2025   *
  * Вариант: 3         *
  * ********************/
  
@@ -11,29 +11,36 @@
 using namespace std;
  
 int main() {
-  double e;
+  double emf;
   double r;
   double R;
+  double R5;
+  double R10;
   double P;
    
-  cout << " e = ";
-  cin >> e;
+  cout << " emf = ";
+  cin >> emf;
   cout << " r = ";
   cin >> r;
   cout << " R = ";
   cin >> R;
+  cout << " R5 = ";
+  cin >> R5;
+  cout << " R10 = ";
+  cin >> R10;
+ 
    
-  while (R < 50) {
-    P = R * pow(e / (R + r), 2);
+  while (R < R5) {
+    P = R * pow(emf / (R + r), 2.0);
     cout << " R = " << R << "  " << " P = " << P << endl;
-    R += 10;
+    R += 10.0;
   }
    
-  while (R <= 300) {
-    P = R * pow(e / (R + r), 2);
+  do {
+    P = R * pow(emf / (R + r), 2.0);
     cout << " R = " << R << "  " << " P = " << P << endl;
-    R += 50;
-  }
+    R += 50.0; } while (R <= R10);
    
   return 0;
+
 }
